24ff0bb1e5989c1d01f6601cc875799fcfc71add
tienbui <tienbui1508@gmail.com>
2023-06-03 19:52:26 +0930

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Tien Bui on 3/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
