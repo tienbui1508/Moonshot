d2db6e9312477dd8e1abd8dde46dd87fdaa4906e
tienbui <tienbui1508@gmail.com>
2023-06-05 00:03:39 +0930

    1st working version

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Tien Bui on 3/6/2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
